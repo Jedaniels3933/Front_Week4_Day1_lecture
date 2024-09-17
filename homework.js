b2e656b6c55ec73a5c40f1b871899251cfcb5b14
Jeremiah Daniels <jedaniels3933@gmail.com>
2024-09-16 22:00:46 -0400

    Final

---
+++ homework.js
@@ -0,0 +1,75 @@
+// Understanding Data Types in JavaScript
+// Task 1: Declare variables to represent student information such as name, age, grade, and attendance status.
+
+const studentName;
+let studentAges;
+var studentGrade;
+let studentAttendance;
+
+// Task 2: Assign sample values to the student information variables.
+
+let studentNames = ["John Doe", "Jane Doe" ,"James Donald" ,"Jenny Doe"];
+let studentAges = [25 , 21 , 22 , 23];
+var studentGrades = ["A", "B", "F", "D"];
+let studentAttendances = [true, false, false, true];
+// James Donald is 22 years old and has a grade of F because of poor attendance. Thats canon now 
+
+// *Task 3:* Display the student information using console.log() statements.
+console.log(studentNames);
+console.log(studentAge);
+console.log(studentGrades);
+console.log(studentAttendances);
+
+
+//  Task 1: Declare variables to store two numeric values for performing arithmetic operations.
+let num1;
+let num2;
+
+// Task 2: Assign sample numeric values to the variables declared in Task 1.
+num1 = 20;
+num2 = 10;
+
+// Task 3: Perform arithmetic operations using the variables declared in Task 1.
+let difference = num1 - num2;
+let quotient = num1 / num2;
+let product = num1 * num2;
+let sum = num1 + num2;
+// Perform arithmetic operations with console.log() statements.
+console.log(sum);
+console.log(quotient);
+console.log(product);
+console.log(difference);
+
+// Task 4: Explore assignment operators and update the values of variables.
+
+console.log("Updatednum1:", num1 += 5);
+console.log("Updatednum2:", num2 -= 5);
+
+// Task 5: Use comparison operators to compare the values of variables.
+
+console.log(num1 == num2);
+console.log(num1 > num2);
+console.log(num1 != num2);
+
+// Task 6: Apply logical operators to combine conditions and display the results.
+
+let isPositive = num1 > 0 && num2 > 0;
+console.log("Are both numbers positive?", isPositive);
+let isEVEN = num1 % 2 == 0 || num2 % 2 == 0;
+console.log("Is at least one number even?", isEVEN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
